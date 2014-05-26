503bb26173bab3f48f62aee0caa4a5cd8a7bc1d5
Joshua Smith <joshuawordsmith@gmail.com>
2014-05-26 14:03:08 -0400

    adding underscore

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
 console.log('\'Allo \'Allo!');
+
+
+//puts info into repo-list via the repo master script
+var showInfo = _.template($('.info-master').text());
+//defines renderObj function
+function renderObj (data, destination){
+		var rendered = showInfo(data);
+		destination.prepend(rendered);
+	}
+
+//grabs the profile info
+$.getJSON('http://gateway.marvel.com:80/v1/public/characters?name=Gambit&apikey=ecbc9156149622b1e7ffc3da73eea458').done(function(data){
+	renderObj(data, $('.container'));
+});
