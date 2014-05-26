bc54a56704e886ae08ed50b02de74859a0e746fb
Joshua Smith <joshuawordsmith@gmail.com>
2014-05-26 14:23:33 -0400

    beestings

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// sets the git hub user the page will display
-var user = 'joshuawordsmith';
-
-
-
 
 
 //puts info into repo-list via the repo master script
@@ -30,12 +25,4 @@ function renderArr (array, destination){
 //grabs the profile info
 $.getJSON('http://gateway.marvel.com:80/v1/public/characters?name=Gambit&apikey=ecbc9156149622b1e7ffc3da73eea458').done(function(data){
 	renderObj(data, $('.info-list'));
-});
-
-
-
-
-//grabs the repo info
-$.getJSON('https://api.github.com/users/' + user + '/repos?client_id=567f27a0a07d1b816db5&client_secret=2076ba9086dc6126360d7ae90e0c062bb951542d').done(function(data){
-	renderArr(data, $('.repo-list'));
 });
